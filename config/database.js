07ce6c8517b3361cff46636376fb91faf9fbe60c
saurabhshr123 <saurabhshr123@gmail.com>
2021-12-12 15:28:41 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'webscrapper'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Service$11'),
+        username: env('DATABASE_USERNAME', 'dbguy'),
+        password: env('DATABASE_PASSWORD', 'Admin$11'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
